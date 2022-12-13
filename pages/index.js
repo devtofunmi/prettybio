5bb947f8852936791e78ba9cb88a98ed37171ae6
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-13 17:42:04 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Index = () => {
   return (
-    <div>
-      
+     <div>
+      <Navbar />
+    <div  className='w-full h-screen px-10'>
+      <main className='flex justify-center mt-40'>
+        <h1  className="text-8xl font-bold">Plenty links is s*#t</h1>
+      </main>
     </div>
+     </div>
   )
 }
 
