ebd696f27129d741a99068f4fec03c6b6df9b137
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-16 21:13:35 +0100

    .

---
+++ components/Features.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FeaturesCard from "./FeaturesCard";
+import FeaturesItem from "./FeaturesItem";
+
+const Features = () => {
+  return (
+    <div>
+      <FeaturesCard />
+    </div>
+  );
+};
+
+export default Features;
