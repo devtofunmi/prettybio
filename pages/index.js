b3847b8b327c1549a9af5a425ea740414e090e82
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-28 09:21:32 +0100

    add gradient as bg and add footer components

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Features from '../components/Features'
+import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 
 import Navbar from '../components/Navbar'
 
 const Index = () => {
   return (
-    <div className="bg-[#f5f8f9]">
+    // bg-[#f5f8f9]
+    <div className="bg-gradient-to-r from-blue-100 to-fuchsia-200 font-abc">
       <Navbar/>
      <Hero/>
      <Features />
+     <Footer />
      </div>
   )
 }
