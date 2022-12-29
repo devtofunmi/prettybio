45f8234434979bfd89fb5cbab10fbe7d301c1d7a
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-29 16:00:07 +0100

    work on Analytics ui

---
+++ components/Analytics.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Analytics = () => {
+  return (
+    <div className="w-full">
+      <div className="lg:w-4/5 w-FULL text-sm lg:text-lg md:w-11/12 rounded-xl   p-10  mt-2">
+        <div className="mt-5">
+          <h1>Page Views</h1>
+          <div className=" mt-5 bg-gray-200 p-5 rounded-md w-28 ">
+            <div className="text-center">
+              <p>500</p>
+              <p className="text-gray-500">Views</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h1>Top Links</h1>
+          <div className="flex mt-5 bg-gray-200 p-5 rounded-md justify-between ">
+            <div>
+              <h1>Twitter</h1>
+              <p className="text-gray-500 text-xs ">
+                https://twitter.com/codebrea_er
+              </p>
+            </div>
+            <div className="text-center">
+              <p>250</p>
+              <p className="text-gray-500">Click</p>
+            </div>
+          </div>
+          <div className="flex mt-5 bg-gray-200 p-5 rounded-md justify-between ">
+            <div>
+              <h1>Github</h1>
+              <p className="text-gray-500 text-xs ">
+                https://github.com/devtofunmi
+              </p>
+            </div>
+            <div className="text-center">
+              <p>100</p>
+              <p className="text-gray-500">Click</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
