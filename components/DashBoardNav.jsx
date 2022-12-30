f0fb204e04b3f77ddb61c3d99e76017928d5d079
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-30 12:01:36 +0100

    navbar on dashboard page

---
+++ components/DashBoardNav.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import GradientBorder from "./GradientBorder";
+
+const DashBoardNav = () => {
+  return (
+    <div className="flex items-center justify-between p-4 bg-white shadow-sm sticky top-0">
+      <div>
+        <Link href="/">
+          <h2 className="text-3xl">PrettyBio</h2>
+        </Link>
+      </div>
+
+      <GradientBorder>
+        <div className="w-10 h-10 rounded-full">
+          <img src={"./assets/jay.png"} alt="features image" />
+        </div>
+      </GradientBorder>
+    </div>
+  );
+};
+
+export default DashBoardNav;
