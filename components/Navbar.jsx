50bb7bc04133c9ae2d3e8114814ffbad1f2ca8e2
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-31 13:07:50 +0100

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,29 @@
+import Link from "next/link";
 import React from "react";
+import GradientBorder from "./GradientBorder";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center">
-      <div className="flex justify-between align-middle bg-white w-10/12 rounded-full py-3 mt-6 px-5 text-black text-3xl">
-        <div>PrettyBio</div>
-        <div className="text-xl space-x-5">
-          <button>Log in</button>
-          <button>Sign up</button>
-        </div>
+    <div className="flex items-center justify-between p-4 lg:p-8">
+      <div>
+        <Link href="/">
+          <h2 className="text-2xl lg:text-3xl">PrettyBio</h2>
+        </Link>
+      </div>
+      <div className=" flex items-center">
+        <Link href="/Login">
+          <button className="bg-transparent text-sm lg:text-lg px-8">
+            Log in
+          </button>
+        </Link>
+
+        <GradientBorder>
+          <Link href="/Signup">
+            <button className="bg-white rounded-full text-sm lg:text-lg px-1 lg:px-4 py-2 ">
+              Sign up
+            </button>
+          </Link>
+        </GradientBorder>
       </div>
     </div>
   );
