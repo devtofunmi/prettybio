0aafffd379f57b2a06bff35bcd345b046947363e
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-31 13:04:12 +0100

    hero section on landing page

---
+++ components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import UserInput from "./UserInput.jsx";
+
+const Hero = () => {
+  return (
+    <div className="mt-20">
+      <h1 className="text-5xl md:text-7xl lg:text-8xl font-abc font-bold mx-auto text-center">
+        Plenty links <br /> is s*#t
+      </h1>
+      <UserInput />
+      <p className="text-lg mt-6 text-center">
+        Effortlessly share all your online links in one place
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
