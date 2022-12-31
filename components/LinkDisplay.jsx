2c1fde48a0bad65e1c9840d2bb6f6e0103588d6c
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-31 13:05:13 +0100

    links in the dashboard page

---
+++ components/LinkDisplay.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const LinkDisplay = [
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+];
+
+export default LinkDisplay;
