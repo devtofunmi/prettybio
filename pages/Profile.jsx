badd8636965cd4b97bc4dae19d6810d768c2442c
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-05-07 11:45:30 +0100

    adjust

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -3,6 +3,7 @@ import ProfileLinks from '../components/ProfileLinks';
 import GradientBorder from '../components/GradientBorder';
 import ShareLinkModal from '../components/ShareLinkModal';
 import { CiShare1 } from "react-icons/ci";
+import Image from 'next/image';
 
 const Profile = () => {
     const [shareModal, setShareModal] = useState(false);
@@ -14,7 +15,7 @@ const Profile = () => {
     <div className=" w-[80%]md:w-[60%] text-sm  lg:w-[50%] rounded-xl  m-auto p-8  mt-2 font-abc ">
       <ShareLinkModal closeModal={closeModal} shareModal={shareModal} />
 
-      <div className='flex justify-start'>
+      <div className="flex justify-start">
         <button
           className="text-xl text-whi px-10"
           onClick={() => {
@@ -22,14 +23,19 @@ const Profile = () => {
           }}
         >
           <div>
-            <CiShare1 className="text-[20px] md:text-[30px] hover:bg-gray-200 "  />
+            <CiShare1 className="text-[20px] md:text-[30px] hover:bg-gray-200 " />
           </div>
         </button>
       </div>
       <div className="flex flex-col items-center mt-5 md:mt-0">
         <GradientBorder>
           <div className="w-20 h-20 md:w-28 md:h-28 rounded-full">
-            <img src={"./assets/blur.png"} alt="features image" />
+            <Image
+              src={"/assets/blur.png"}
+              alt="features image"
+              width={500}
+              height={500}
+            />
           </div>
         </GradientBorder>
 
@@ -38,9 +44,12 @@ const Profile = () => {
       </div>
 
       <div className="mt-5 ">
-        {ProfileLinks.map((data) => (
-          <div className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white  text-center">
-            <a href={data.linkUrl} target="_blank">
+        {ProfileLinks.map((data, k) => (
+          <div
+            className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white  text-center"
+            key={k}
+          >
+            <a href={data.linkUrl} target="_blank" rel="noreferrer">
               <h1>{data.linkName}</h1>
             </a>
           </div>
