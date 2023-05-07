ff088159af28cca675e51302694407a2c4e6932e
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-05-07 11:25:08 +0100

    add the profile link components

---
+++ components/ProfileLinks.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProfileLinks = [
+  {
+    linkName: "Twitter",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "Github",
+    linkUrl: "github.com/devtofunmi",
+  },
+  {
+    linkName: "whatsapp",
+    linkUrl: "wa.me/09072905477",
+  },
+  {
+    linkName: "Facebook",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+  {
+    linkName: "Linkedin",
+    linkUrl: "twitter.com/codebreak_er",
+  },
+ 
+];
+
+export default ProfileLinks;
