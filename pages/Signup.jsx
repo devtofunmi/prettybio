3a6ed32448d8fa910ae36c1cef85352a0e26715e
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-05-10 12:18:58 +0100

    ...

--- pages/Signup.jsx
+++ pages/Signup.jsx
@@ -6,7 +6,7 @@ import { supabase } from "../supabaseClient";
 import { useRouter } from "next/router";
 
 const Signup = () => {
-  const [username, setUserName] = useState("");
+  const [email, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
@@ -20,7 +20,7 @@ const Signup = () => {
     }
 
     let { data, error } = await supabase.auth.signUp({
-      username: username,
+      email: email,
       password: password,
       confirmPassword: confirmPassword,
      
@@ -30,7 +30,7 @@ const Signup = () => {
 
     setLoading(true);
     setTimeout(() => {
-      if (!username) {
+      if (!email) {
         setError("Please enter your username");
       } else if (!password) {
         setError("Please enter your password");
@@ -38,11 +38,13 @@ const Signup = () => {
         setError("Please confirm your password");
       }  else if (error) {
         setError(error.message);
+      }  else if (password != confirmPassword) {
+        setError("password is not the same with confirm password");
       } else if (data.error) {
         setError(data.error.message);
       } else {
+        setSuccess("Login succesful");
         router.push("/Login");
-        setError("Login succesful");
       }
       setLoading(false);
     }, 1000);
@@ -60,13 +62,18 @@ const Signup = () => {
       {error && (
         <div className="fixed top-0 left-0 right-0 items-center bg-red-500 text-white p-4 ">
           <div className="flex justify-between">
-          <p>{error}</p>
-          <button
-            className=" px-2 py-1"
-            onClick={() => setError("")}
-          >
-            X
-          </button>
+            <p>{error}</p>
+            <button className=" px-2 py-1" onClick={() => setError("")}>
+              X
+            </button>
+          </div>
+        </div>
+      )}
+
+      {success && (
+        <div className="fixed top-0 left-0 right-0 items-center bg-green-500 text-white p-4 ">
+          <div className="text-center">
+          <p>{success}</p>
           </div>
         </div>
       )}
@@ -74,7 +81,7 @@ const Signup = () => {
       <div className=" w-5/6 md:w-2/4 text-sm  lg:w-4/12 rounded-xl  m-auto p-14  mt-2">
         <div className="flex flex-col mt-3 justify-center ">
           <input
-            type="username"
+            type="email"
             class="placeholder-black focus:outline-none focus:border-blue-700  border border-gray-400 rounded-md py-2 px-4 block w-full "
             placeholder="Username"
             onChange={(e) => {
@@ -105,16 +112,14 @@ const Signup = () => {
           </div>
         </div>
         <div className="mt-5 justify-center items-center flex ">
-         
-            <GradientBorder>
-              <button
-                className=" px-14  lg:px-32 md:px-20 py-2 bg-transparent  text-white text-base rounded-full "
-                onClick={handleSubmit}
-              >
-                {loading ? <p>loading...</p> : <p>SIGN UP </p>}
-              </button>
-            </GradientBorder>
-         
+          <GradientBorder>
+            <button
+              className=" px-14  lg:px-32 md:px-20 py-2 bg-transparent  text-white text-base rounded-full "
+              onClick={handleSubmit}
+            >
+              {loading ? <p>loading...</p> : <p>SIGN UP </p>}
+            </button>
+          </GradientBorder>
         </div>
       </div>
     </div>
