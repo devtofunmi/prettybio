2f8991bc8bc35ae776c59414a2c55e4e12c650b7
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-05-11 10:49:55 +0100

    ..

--- pages/Setup.jsx
+++ pages/Setup.jsx
@@ -4,9 +4,19 @@ import React, { useRef, useState } from "react";
 import { BsCamera } from "react-icons/bs";
 import DashBoardNav from "../components/DashBoardNav";
 import GradientBorder from "../components/GradientBorder";
+import { useRouter } from "next/router";
+import { supabase } from "../supabaseClient";
 const Setup = () => {
   const [image, setImage] = useState("");
   const fileInputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  const [bio, setBio] = useState("");
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
   function uploadImage(e) {
     setImage(e.target.files[0]);
     console.log(e.target.files);
@@ -14,6 +24,37 @@ const Setup = () => {
   const handleButtonClick = () => {
     fileInputRef.current.click();
   };
+
+  const setUp = async () => {
+    
+      const { data, error } = await supabase
+       .from("setup")
+       .insert([{ name: name }, { link: link }, { bio: bio}]);
+        console.log(data, error);
+
+        setLoading(true);
+        setTimeout(() => {
+          if (!name) {
+            setError("Enter your name");
+          } else if (!link) {
+            setError("Enter your link");
+          } else if (!bio) {
+            setError("Enter your bio");
+          } else if (data.error) {
+            setError(data.error.message);
+          } else {
+            setSuccess("Setup succesful");
+            router.push("/Dashboard");
+          }
+          setLoading(false);
+        }, 1000);
+
+  }
+
+
+  function handleSubmit() {
+    setUp();
+  }
   return (
     <div className="font-abc">
       <Link href="/">
@@ -22,6 +63,26 @@ const Setup = () => {
       <div className="text-center">
         <h1 className="text-3xl">Setup your page</h1>
       </div>
+
+      {error && (
+        <div className="fixed top-0 left-0 right-0 items-center bg-red-500 text-white p-4 ">
+          <div className="flex justify-between">
+            <p>{error}</p>
+            <button className=" px-2 py-1" onClick={() => setError("")}>
+              X
+            </button>
+          </div>
+        </div>
+      )}
+
+      {success && (
+        <div className="fixed top-0 left-0 right-0 items-center bg-green-500 text-white p-4 ">
+          <div className="text-center">
+            <p>{success}</p>
+          </div>
+        </div>
+      )}
+
 
       <div className=" w-11/12 md:w-2/4   lg:w-4/12 rounded-xl  m-auto p-14  mt-2">
         <div className="flex flex-col mt-3 justify-center">
@@ -40,6 +101,9 @@ const Setup = () => {
               type="password"
               class="placeholder-black focus:outline-none focus:border-blue-700  border border-gray-400 rounded-md py-2 px-4 block w-full"
               placeholder="Name"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
             />
             <div className="flex text-sm items-center text-black bg-white mt-5 border pl-4 rounded-md ">
               <p>prettybio.com/</p>
@@ -47,6 +111,9 @@ const Setup = () => {
                 type="text"
                 placeholder="your name"
                 className="py-2 bg-transparent outline-none "
+                onChange={(e) => {
+                  setLink(e.target.value);
+                }}
               />
             </div>
 
@@ -55,17 +122,21 @@ const Setup = () => {
                 type="password"
                 class="placeholder-black focus:outline-none focus:border-blue-700  border border-gray-400 rounded-md py-2 px-4 block w-full"
                 placeholder="Bio"
+                onChange={(e) => {
+                  setBio(e.target.value);
+                }}
               />
             </div>
           </div>
           <div className="mt-5 justify-center items-center flex ">
-            <Link href="/Dashboard">
-              <GradientBorder>
-                <button className=" px-14  lg:px-32 md:px-20 py-2 bg-transparent  text-white text-base rounded-full ">
-                  Continue
-                </button>
-              </GradientBorder>
-            </Link>
+            <GradientBorder>
+              <button
+                className=" px-14  lg:px-32 md:px-20 py-2 bg-transparent  text-white text-base rounded-full "
+                onClick={handleSubmit}
+              >
+                {loading ? <p>loading...</p> : <p>Continue</p>}
+              </button>
+            </GradientBorder>
           </div>
         </div>
       </div>
