e5925bd2770a23ca277d96c87cdc8a998690cbf1
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-05-30 04:48:19 +0100

    ..

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import React, { useState } from "react";
 import { supabase } from "../supabaseClient";
 import GradientBorder from "../components/GradientBorder";
@@ -6,58 +5,73 @@ import Navbar from "../components/Navbar";
 import { useRouter } from "next/router";
 
 const Login = () => {
-const [username, setUserName] = useState("");
-const [password, setPassword] = useState("");
-const [loading, setLoading] = useState(false);
-const router = useRouter();
-const [error, setError] = useState("");
-const [success, setSuccess] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-const logIn = async () => {
+  const logIn = async () => {
+    const { data, error } = await supabase
+      .from("users")
+      .select("id, image, link, username, bio, setup_complete")
+      .eq("username", username)
+      .eq("password", password);
 
-  const { data, error } = await supabase
-    .from("users")
-    .select()
-    .eq("username", username)
-    .eq("password", password);
-
-  setLoading(true);
-    setTimeout(() => {
+    setLoading(true);
+    setTimeout(async () => {
       if (!username) {
         setError("Enter your email");
       } else if (!password) {
         setError("Enter your password");
       } else if (error) {
         setError(error.message);
-      }else if (data.length === 0) {
-        setError("incorrect username or password");
-
-        console.log("No matching user found");
-      }
-       else if (data.length === 1) {
+      } else if (data.length === 0) {
+        setError("Incorrect username or password");
+      } else if (data.length === 1) {
         const user = data[0];
         console.log("User data:", user);
-        setSuccess("Login succesful");
-        router.push("/Setup");
+        setSuccess("Login successful");
+         setTimeout(() => {
+           setSuccess("");
+         }, 2000);
+
+        const setupComplete = user.setup_complete;
+
+        if (setupComplete === false) {
+          const storedData = localStorage.getItem("data");
+          const dataArray = JSON.parse(storedData);
+          const userId = dataArray[0]?.id;
+
+          const { data, error } = await supabase
+            .from("users")
+            .update({ setup_complete: true })
+            .eq("id", userId);
+
+          if (error) {
+            console.error("Error updating setup status:", error);
+            return;
+          }
+
+          console.log("Setup status updated successfully");
+          router.push("/Setup");
+        } else if (setupComplete === true) {
+          router.push("/Dashboard");
+        }
+      } else {
+        console.log("Multiple users found with the same username and password");
       }
-       else {
-         console.log(
-           "Multiple users found with the same username and password"
-         );
-       
-      }
-      
+
       setLoading(false);
+    }, 1000);
+  };
 
-    },1000)
+  function handleSubmit() {
+    setError("");
+    logIn();
+  }
 
-
-}
-
-function handleSubmit() {
-  setError("");
-  logIn();
-}
   return (
     <div className="font-abc">
       <Navbar />
@@ -67,7 +81,7 @@ function handleSubmit() {
         <div className="fixed top-0 left-0 right-0 items-center bg-red-500 text-white p-4 ">
           <div className="flex justify-between">
             <p>{error}</p>
-            <button className=" px-2 py-1" onClick={() => setError("")}>
+            <button className="px-2 py-1" onClick={() => setError("")}>
               X
             </button>
           </div>
@@ -82,11 +96,11 @@ function handleSubmit() {
         </div>
       )}
 
-      <div className=" w-[450px] md:w-2/4 text-sm  lg:w-4/12 rounded-xl  m-auto p-14  mt-2">
+      <div className="w-[450px] md:w-2/4 text-sm lg:w-4/12 rounded-xl m-auto p-14 mt-2">
         <div className="flex flex-col mt-3 ">
           <input
             type="text"
-            class="placeholder-black focus:outline-none focus:border-blue-700  border border-gray-400 rounded-md py-3 px-4 block w-full"
+            className="placeholder-black focus:outline-none focus:border-blue-700 border border-gray-400 rounded-md py-3 px-4 block w-full"
             placeholder="Username"
             onChange={(e) => {
               setUserName(e.target.value);
@@ -96,7 +110,7 @@ function handleSubmit() {
           <div className="mt-5">
             <input
               type="password"
-              class="placeholder-black focus:outline-none focus:border-blue-700  border border-gray-400 rounded-md py-3 px-4 block w-full mt-3"
+              className="placeholder-black focus:outline-none focus:border-blue-700 border border-gray-400 rounded-md py-3 px-4 block w-full mt-3"
               placeholder="Password"
               onChange={(e) => {
                 setPassword(e.target.value);
@@ -104,17 +118,15 @@ function handleSubmit() {
             />
           </div>
         </div>
-        <div className="mt-5 justify-center items-center flex ">
-          
-            <GradientBorder>
-              <button
-                className=" px-14  lg:px-32 md:px-20 py-3 bg-transparent  text-white text-base rounded-full "
-                onClick={handleSubmit}
-              >
-                {loading ? <p>loading...</p> : <p>LOG IN </p>}
-              </button>
-            </GradientBorder>
-         
+        <div className="mt-5 justify-center items-center flex">
+          <GradientBorder>
+            <button
+              className="px-14 lg:px-32 md:px-20 py-3 bg-transparent text-white text-base rounded-full"
+              onClick={handleSubmit}
+            >
+              {loading ? <p>loading...</p> : <p>LOG IN</p>}
+            </button>
+          </GradientBorder>
         </div>
       </div>
     </div>
