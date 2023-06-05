ce374f5aa43440e1ceafa272e1d8cb7e71b5a753
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-05 05:48:23 +0100

    signup navbar component

---
+++ components/SignupNavbar.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+import GradientBorder from "./GradientBorder";
+
+const SignupNavbar = () => {
+  return (
+    <div className="flex items-center justify-between p-4 lg:p-8">
+      <div>
+        <Link href="/">
+          <h2 className="text-2xl lg:text-3xl">PrettyBio</h2>
+        </Link>
+      </div>
+      <div>
+        <GradientBorder>
+          <Link href="/Login">
+            <button className="bg-transparent text-sm lg:text-lg px-8">
+              Log in
+            </button>
+          </Link>
+        </GradientBorder>
+      </div>
+    </div>
+  );
+};
+
+export default SignupNavbar;
