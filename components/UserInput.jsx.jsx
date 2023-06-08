5e7404db0cef819e2b567646552711b8f51a7958
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-08 18:15:53 +0100

    adjust py

--- components/UserInput.jsx.jsx
+++ components/UserInput.jsx.jsx
@@ -8,13 +8,13 @@ const UserInput = () => {
       <div className="flex items-center text-black justify-center w-full  md:w-[500px] lg:w-1/2  px-5  my-auto mx-auto text-sm  lg:text-lg rounded-md mt-10">
         <GradientBorder>
           <div className="flex items-center bg-white ml-1 pl-2 md:py-1 xm:text-[10px]  text-sm rounded-full">
-            <p className="py-1  text-[12px] md:text-base lg:text-xl ">
+            <p className="py-3  text-[12px] md:text-base lg:text-xl ">
               prettybio.com/
             </p>
             <input
               type="text"
               placeholder="yourname"
-              className="h-[2rem] md:h-[3rem] rounded-full outline-none w-[50%] md:w-[60%] lg:w-[70%] text-[12px] lg:text-base bg-transparent px-0 md:px-0"
+              className="h-[2rem] md:h-[3rem] rounded-full  outline-none w-[50%] md:w-[60%] lg:w-[70%] text-[10px] lg:text-base bg-transparent px-0 md:px-0"
             />
           </div>
           <Link href="/Signup">
