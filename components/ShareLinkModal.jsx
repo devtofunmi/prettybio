9b60aa9a271e8a4e641385f23bd00ecf31b600af
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-08 06:52:23 +0100

    add afunction for the link

--- components/ShareLinkModal.jsx
+++ components/ShareLinkModal.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
 import { MdOutlineClose } from "react-icons/md";
 
-const ShareLinkModal = ({ shareModal, closeModal }) => {
+const ShareLinkModal = ({ shareModal, closeModal, userLinkName }) => {
+  const handleTwitterShare = () => {
+    const pageLink = `https://prettybio.netlify.app/${userLinkName}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+      pageLink
+    )}`;
+    window.open(twitterUrl, "_blank");
+  };
+
+  const handleEmailShare = () => {
+    const pageLink = `https://prettybio.netlify.app/${userLinkName}`;
+    const subject = "Check out this page";
+    const emailBody = `I wanted to share this page with you: ${pageLink}`;
+    const emailUrl = `mailto:?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(emailBody)}`;
+    window.location.href = emailUrl;
+  };
+
+  const handleFacebookShare = () => {
+    const pageLink = `https://prettybio.netlify.app/${userLinkName}`;
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      pageLink
+    )}`;
+    window.open(facebookUrl, "_blank");
+  };
+
+  const handleWhatsAppShare = () => {
+    const pageLink = `https://prettybio.netlify.app/${userLinkName}`;
+    const whatsappUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(
+      pageLink
+    )}`;
+    window.open(whatsappUrl, "_blank");
+  };
+
   return (
     <div>
       <div
@@ -9,22 +43,39 @@ const ShareLinkModal = ({ shareModal, closeModal }) => {
           shareModal ? "flex" : "hidden"
         }`}
       >
-        <div className="w-4/5 md:w-2/4   lg:w-[40%]  rounded-xl bg-white  m-auto p-8  mt-40">
+        <div className="w-4/5 md:w-2/4 lg:w-[40%] rounded-xl bg-white m-auto p-8 mt-40">
           <div className="flex justify-end">
             <button onClick={() => closeModal()}>
-              <MdOutlineClose className='text-[20px] hover:bg-gray-200' />
+              <MdOutlineClose className="text-[20px] hover:bg-gray-200" />
             </button>
           </div>
           <div>
-            <p className="text-center text-[18px] md:text-[20px] mt-3 ">Share this Bio Link</p>
+            <p className="text-center text-[18px] md:text-[20px] mt-3 ">
+              Share this Bio Link
+            </p>
             <div>
-              <p className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white  text-center">
+              <p
+                className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white text-center"
+                onClick={handleTwitterShare}
+              >
                 Share on Twitter
               </p>
-              <p className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white  text-center">
+              <p
+                className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white text-center"
+                onClick={handleWhatsAppShare}
+              >
+                Share on WhatsApp
+              </p>
+              <p
+                className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white text-center"
+                onClick={handleFacebookShare}
+              >
                 Share on Facebook
               </p>
-              <p className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white  text-center">
+              <p
+                className="bg-gray-200 my-5 p-5 rounded-xl cursor-pointer hover:bg-pink-500 hover:text-white text-center"
+                onClick={handleEmailShare}
+              >
                 Share via Email
               </p>
             </div>
@@ -35,4 +86,4 @@ const ShareLinkModal = ({ shareModal, closeModal }) => {
   );
 };
 
-export default ShareLinkModal
+export default ShareLinkModal;
