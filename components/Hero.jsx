6d66bbec2eab3e044edaca789c009d41f49ebba5
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-11 14:26:02 +0100

    add aos animation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,12 +4,18 @@ import UserInput from "./UserInput.jsx";
 const Hero = () => {
   return (
     <div className="mt-20">
-      <h1 className="text-5xl md:text-7xl lg:text-8xl font-abc font-bold mx-auto text-center">
+      <h1
+        data-aos="zoom-in"
+        className="text-5xl md:text-7xl lg:text-8xl font-abc font-bold mx-auto text-center"
+      >
         Plenty links <br /> is s*#t
       </h1>
-      
+
       <UserInput />
-      <p className="px-[30px] text-[18px] md:text-lg mt-6 text-center">
+      <p
+        data-aos="fade-down"
+        className="px-[30px] text-[18px] md:text-lg mt-6 text-center"
+      >
         Effortlessly share all your online links in one place
       </p>
     </div>
