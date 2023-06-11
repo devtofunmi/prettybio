2acdfcd69fc8e639bf6d7fabb9f9bfe4e4bb448e
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-11 14:25:35 +0100

    add aos animation

--- components/FeaturesCard.jsx
+++ components/FeaturesCard.jsx
@@ -10,13 +10,19 @@ const FeaturesCard = () => {
             card.isInverted
               ? "flex-row-reverse lg:flex-row-reverse"
               : "flex-row"
-          }`} key={i}
+          }`}
+          key={i}
         >
-          <div className="flex flex-col mt-28 w-full lg:w-5/6">
+          <div
+            data-aos="zoom-in-right"
+            className="flex flex-col mt-28 w-full lg:w-5/6"
+          >
             <div className="text-4xl md:text-6xl">{card.heading}</div>
             <div className=" mt-8 text-xl">{card.subheading}</div>
           </div>
-          <div className="w-5/6">{card.image}</div>
+          <div data-aos="zoom-in-left" className="w-5/6">
+            {card.image}
+          </div>
         </div>
       ))}
     </div>
