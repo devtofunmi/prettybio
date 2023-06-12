a4597e638c202acc8568616a69253038f1958d82
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-12 15:44:11 +0100

    add color and screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
- content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "320px",
+      md: "640px",
+      xmd: "840px",
+      lg: "1024px",
+      xl: "1280px",
+      _2k: "2000px",
+    },
     extend: {
-      fontFamily: { },
-      fontFamily:{
-        abc:["Unbounded" ,"cursive"]
-      }
+      fontFamily: {
+        abc: ["Unbounded", "cursive"],
+      },
+      colors: {
+        textHover: "#effbce",
+        text: "#d4d7d9",
+        btntext: "#a6acae",
+        primary: "#2b2f3b",
+        secondary: "#b997f7",
+        btntext: "#3d4042",      
+      },
     },
   },
   plugins: [],
-}
+};
