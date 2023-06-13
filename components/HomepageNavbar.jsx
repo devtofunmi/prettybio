af9fdb6a1d36a8703fe9536ea8692e3b7c08774d
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-13 05:34:01 +0100

    ..

--- components/HomepageNavbar.jsx
+++ components/HomepageNavbar.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import GradientBorder from "./GradientBorder";
 const HomepageNavbar = () => {
   return (
-    <div className="flex items-center justify-between p-4 lg:p-8">
+    <div className="flex items-center justify-between p-4 lg:p-8 text-text">
       <div>
         <Link href="/">
-          <h2 className="text-2xl lg:text-3xl">PrettyBio</h2>
+          <h2 className="text-2xl lg:text-3xl hover:text-textHover">PrettyBio</h2>
         </Link>
       </div>
       <div className='flex items-center justify-center'>
@@ -19,7 +19,7 @@ const HomepageNavbar = () => {
         </div>
         <GradientBorder>
           <Link href="/Signup">
-            <button className="bg-white rounded-full text-sm lg:text-lg px-1 lg:px-4 py-2 ">
+            <button className="bg-transparent rounded-full text-sm lg:text-lg px-1 lg:px-4 py-2 text-btntext ">
               Sign up
             </button>
           </Link>
