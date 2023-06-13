021b56f57d9fcf5aa21b3c4960185f0b8802a59e
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-13 05:32:56 +0100

    ..

--- components/FeaturesCard.jsx
+++ components/FeaturesCard.jsx
@@ -3,7 +3,7 @@ import FeaturesItem from "./FeaturesItem";
 
 const FeaturesCard = () => {
   return (
-    <div className="flex flex-col  items-center mt-20 px-5 md:px-10">
+    <div className="flex flex-col  items-center mt-20 px-5 md:px-10 text-text">
       {FeaturesItem.map((card, i) => (
         <div
           className={`flex flex-col lg:flex-row ${
