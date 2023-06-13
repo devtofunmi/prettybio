4f1f500dff6ab1d1578186cb5f80671b63c903f6
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-13 05:34:16 +0100

    ..

--- components/LinkLoadingSpinner.jsx
+++ components/LinkLoadingSpinner.jsx
@@ -10,16 +10,16 @@ const LinkLoadingSpinner = () => {
         viewBox="0 0 48 48"
         className="md:w-[100px] md:h-[100px] w-[50px] h-[50px]  animate-spin"
       >
-        <circle cx="24" cy="4" r="4" fill="#D83C8D" />
-        <circle cx="12.19" cy="7.86" r="3.7" fill="#D83C8D" />
-        <circle cx="5.02" cy="17.68" r="3.4" fill="#D83C8D" />
-        <circle cx="5.02" cy="30.32" r="3.1" fill="#D83C8D" />
-        <circle cx="12.19" cy="40.14" r="2.8" fill="#D83C8D" />
-        <circle cx="24" cy="44" r="2.5" fill="#D83C8D" />
-        <circle cx="35.81" cy="40.14" r="2.2" fill="#D83C8D" />
-        <circle cx="42.98" cy="30.32" r="1.9" fill="#D83C8D" />
-        <circle cx="42.98" cy="17.68" r="1.6" fill="#D83C8D" />
-        <circle cx="35.81" cy="7.86" r="1.3" fill="#D83C8D" />
+        <circle cx="24" cy="4" r="4" fill="#d4d7d9" />
+        <circle cx="12.19" cy="7.86" r="3.7" fill="#d4d7d9" />
+        <circle cx="5.02" cy="17.68" r="3.4" fill="#d4d7d9" />
+        <circle cx="5.02" cy="30.32" r="3.1" fill="#d4d7d9" />
+        <circle cx="12.19" cy="40.14" r="2.8" fill="#d4d7d9" />
+        <circle cx="24" cy="44" r="2.5" fill="#d4d7d9" />
+        <circle cx="35.81" cy="40.14" r="2.2" fill="#d4d7d9" />
+        <circle cx="42.98" cy="30.32" r="1.9" fill="#d4d7d9" />
+        <circle cx="42.98" cy="17.68" r="1.6" fill="#d4d7d9" />
+        <circle cx="35.81" cy="7.86" r="1.3" fill="#d4d7d9" />
       </svg>
     </div>
   );
--- components/LoadingSpinner.jsx
+++ components/LoadingSpinner.jsx
@@ -7,20 +7,19 @@ const LoadingSpinner = () => {
       <svg
         id="svg-spinner"
         xmlns="http://www.w3.org/2000/svg"
-       
         viewBox="0 0 48 48"
         className="w-[80px] h-6  animate-spin"
       >
-        <circle cx="24" cy="4" r="4" fill="#fff" />
-        <circle cx="12.19" cy="7.86" r="3.7" fill="#fffbf2" />
-        <circle cx="5.02" cy="17.68" r="3.4" fill="#fef7e4" />
-        <circle cx="5.02" cy="30.32" r="3.1" fill="#fef3d7" />
-        <circle cx="12.19" cy="40.14" r="2.8" fill="#feefc9" />
-        <circle cx="24" cy="44" r="2.5" fill="#feebbc" />
-        <circle cx="35.81" cy="40.14" r="2.2" fill="#fde7af" />
-        <circle cx="42.98" cy="30.32" r="1.9" fill="#fde3a1" />
-        <circle cx="42.98" cy="17.68" r="1.6" fill="#fddf94" />
-        <circle cx="35.81" cy="7.86" r="1.3" fill="#fcdb86" />
+        <circle cx="24" cy="4" r="4" fill="#d4d7d9" />
+        <circle cx="12.19" cy="7.86" r="3.7" fill="#d4d7d9" />
+        <circle cx="5.02" cy="17.68" r="3.4" fill="#d4d7d9" />
+        <circle cx="5.02" cy="30.32" r="3.1" fill="#d4d7d9" />
+        <circle cx="12.19" cy="40.14" r="2.8" fill="#d4d7d9" />
+        <circle cx="24" cy="44" r="2.5" fill="#d4d7d9" />
+        <circle cx="35.81" cy="40.14" r="2.2" fill="#d4d7d9" />
+        <circle cx="42.98" cy="30.32" r="1.9" fill="#d4d7d9" />
+        <circle cx="42.98" cy="17.68" r="1.6" fill="#d4d7d9" />
+        <circle cx="35.81" cy="7.86" r="1.3" fill="#d4d7d9" />
       </svg>
     </div>
   );
