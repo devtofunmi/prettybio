fb7005f1dfd652f533a97140e72404ffb65fe705
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-17 11:15:02 +0100

    change the gradient color

--- components/GradientBorder.jsx
+++ components/GradientBorder.jsx
@@ -6,7 +6,7 @@ const GradientBorder = ({ children }) => {
       className="rounded-full p-0.5 fit-content flex items-center justify-center w-fit"
       style={{
         background:
-          "linear-gradient(275.79deg, #e3d4ff  5.39%,#effbce   70.00%)",
+          "linear-gradient(275.79deg, #93c3c4  5.39%,#f2cfad   70.00%)",
       }}
     >
       <div className=" rounded-full flex items-center">{children}</div>
