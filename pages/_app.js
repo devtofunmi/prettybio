5fda6a618c3e675a80ad4e686a01c9a2b796b86f
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-19 11:20:01 +0100

    add a new svg

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import '../styles/globals.css'
-
 import { useEffect } from 'react';
 import { initializeAOS } from '../utils/aos';
+import { ThemeProvider } from "next-themes"
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     initializeAOS();
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+  <ThemeProvider enableSystem={true} attribute="class">
+  <Component {...pageProps} />;
+  </ThemeProvider>
+  )
 }
 
 export default MyApp;
