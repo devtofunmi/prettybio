496c1a73059bfb026ceb7ece026b4eda83c5de7c
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-22 09:38:13 +0100

    add a style for the light mode

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserInput from "./UserInput.jsx";
+import { useTheme } from "next-themes";
 
 const Hero = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+
   return (
-    <div className="mt-20 text-text">
+    <div className={`text-${currentTheme === "dark" ? "text" : "black"} mt-20`}>
       <h1
         data-aos="zoom-in"
         className="text-5xl md:text-7xl lg:text-8xl font-abc font-bold mx-auto text-center"
