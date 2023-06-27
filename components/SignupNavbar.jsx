507c2b4d5b0c6c7a7b63a03781629fe3c1b89e56
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-27 09:49:17 +0100

    add a style for the light mode

--- components/SignupNavbar.jsx
+++ components/SignupNavbar.jsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import GradientBorder from "./GradientBorder";
 
 const SignupNavbar = () => {
+   const { systemTheme, theme, setTheme } = useTheme();
+   const [mounted, setMounted] = useState(false);
+
+   useEffect(() => {
+     setMounted(true);
+   }, []);
+
+   if (!mounted) return null;
+   const currentTheme = theme === "system" ? systemTheme : theme;
   return (
-    <div className="flex items-center justify-between p-4 lg:p-8 text-text">
+    <div
+      className={`${
+        currentTheme === "dark"
+          ? "flex items-center justify-between p-4 lg:p-8 text-text"
+          : "flex items-center justify-between p-4 lg:p-8 text-black"
+      }`}
+    >
       <div>
         <Link href="/">
           <h2 className="text-2xl lg:text-3xl">PrettyBio</h2>
@@ -13,7 +29,14 @@ const SignupNavbar = () => {
       <div>
         <GradientBorder>
           <Link href="/Login">
-            <button className="bg-transparent text-sm lg:text-lg px-8 py-2 text-btntext">
+            <button
+              className={`${
+                currentTheme === "dark"
+                  ? "bg-transparent text-sm lg:text-lg px-8 py-2 text-btntext"
+                  : "bg-transparent text-sm lg:text-lg px-8 py-2 text-black"
+              }`}
+            
+            >
               Log in
             </button>
           </Link>
