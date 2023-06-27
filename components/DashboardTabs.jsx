579e2c68121cbe7d2b324cc7e305a14984699171
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-27 09:48:18 +0100

    add a style for the light mode

--- components/DashboardTabs.jsx
+++ components/DashboardTabs.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import Analytics from "./Analytics";
 import Bio from "./Bio";
 import Links from "./Links";
@@ -6,7 +7,8 @@ import Settings from "./Settings";
 
 const DashboardTabs = () => {
   const [activeTab, setActiveTab] = useState(1);
-
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const tabButtons = [
     {
       id: 1,
@@ -25,31 +27,54 @@ const DashboardTabs = () => {
       title: "Bio",
     },
   ];
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
-    <div className="flex flex-col lg:p-10 p-0 pt-5 md:p-5 ">
+    <div className="flex flex-col lg:p-10 p-0 pt-5 md:p-5">
       <div className="flex w-full overflow-x-scroll">
-        {tabButtons.map((tab, t) => (
-          <div className="p-0  lg:p-5 md:p-1 text-lg lg:text-xl" key={t}>
+        {tabButtons.map((tab) => (
+          <div className="p-0 lg:p-5 md:p-1 text-lg lg:text-xl" key={tab.id}>
             <button
               className={
                 activeTab === tab.id
-                  ? "text-text   rounded-md "
-                  : "  text-gray-500 " + "relative"
+                  ? `${
+                      currentTheme === "dark"
+                        ? "text-text rounded-md"
+                        : "text-black rounded-md"
+                    }`
+                  : `${
+                      currentTheme === "dark"
+                        ? "text-gray-500 relative"
+                        : "text-gray-500"
+                    }`
               }
               onClick={() => setActiveTab(tab.id)}
             >
               {tab.title}
               <div
                 className={`w-36 h-1 ${
-                  activeTab === tab.id ? "bg-text " : "bg-transparent"
+                  activeTab === tab.id
+                    ? `${
+                        currentTheme === "dark"
+                          ? "bg-text"
+                          : "bg-gray-500"
+                      }`
+                    : `${
+                        currentTheme === "dark"
+                          ? "bg-transparent"
+                          : "bg-transparent"
+                      }`
                 }`}
               ></div>
             </button>
           </div>
         ))}
       </div>
-      {/* <div className="w-96 h-1 bg-yellow-700"></div> */}
       <div>
         {activeTab === 1 && <Links />}
         {activeTab === 2 && <Analytics />}
