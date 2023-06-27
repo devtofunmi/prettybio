6e6d5c383177c1f1b768d94004434d0cbfdd12a2
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-27 09:49:09 +0100

    add a style for the light mode

--- components/ShareLinkModal.jsx
+++ components/ShareLinkModal.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import { MdOutlineClose } from "react-icons/md";
 
 const ShareLinkModal = ({ shareModal, closeModal, userLinkName }) => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const handleTwitterShare = () => {
     const pageLink = `https://prettybio.netlify.app/${userLinkName}`;
     const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
@@ -36,6 +39,14 @@ const ShareLinkModal = ({ shareModal, closeModal, userLinkName }) => {
     window.open(whatsappUrl, "_blank");
   };
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+
   return (
     <div>
       <div
@@ -43,10 +54,22 @@ const ShareLinkModal = ({ shareModal, closeModal, userLinkName }) => {
           shareModal ? "flex" : "hidden"
         }`}
       >
-        <div className="w-4/5 md:w-2/4 lg:w-[40%] rounded-xl bg-[#202125] text-text m-auto p-8 mt-40">
+        <div
+          className={`${
+            currentTheme === "dark"
+              ? "w-4/5 md:w-2/4 lg:w-[40%] rounded-xl bg-[#202125] text-text m-auto p-8 mt-40"
+              : "w-4/5 md:w-2/4 lg:w-[40%] rounded-xl bg-[#f7f7f7] text-black m-auto p-8 mt-40"
+          } `}
+        >
           <div className="flex justify-end">
             <button onClick={() => closeModal()}>
-              <MdOutlineClose className="text-[20px] hover:bg-btntext" />
+              <MdOutlineClose
+                className={`${
+                  currentTheme === "dark"
+                    ? "text-[20px] hover:bg-btntext"
+                    : "text-[20px]"
+                } `}
+              />
             </button>
           </div>
           <div>
@@ -55,25 +78,41 @@ const ShareLinkModal = ({ shareModal, closeModal, userLinkName }) => {
             </p>
             <div>
               <p
-                className="bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                className={`${
+                  currentTheme === "dark"
+                    ? "bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                    : "bg-[#f7f7f7] shadow-md my-5 p-5 rounded-xl cursor-pointer text-center"
+                } `}
                 onClick={handleTwitterShare}
               >
                 Share on Twitter
               </p>
               <p
-                className="bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                className={`${
+                  currentTheme === "dark"
+                    ? "bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                    : "bg-[#f7f7f7] shadow-md my-5 p-5 rounded-xl cursor-pointer text-center"
+                } `}
                 onClick={handleWhatsAppShare}
               >
                 Share on WhatsApp
               </p>
               <p
-                className="bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                className={`${
+                  currentTheme === "dark"
+                    ? "bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                    : "bg-[#f7f7f7] shadow-md my-5 p-5 rounded-xl cursor-pointer text-center"
+                } `}
                 onClick={handleFacebookShare}
               >
                 Share on Facebook
               </p>
               <p
-                className="bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                className={`${
+                  currentTheme === "dark"
+                    ? "bg-[#303135] my-5 p-5 rounded-xl cursor-pointer hover:bg-btntext hover:text-white text-center"
+                    : "bg-[#f7f7f7] shadow-md my-5 p-5 rounded-xl cursor-pointer text-center"
+                } `}
                 onClick={handleEmailShare}
               >
                 Share via Email
