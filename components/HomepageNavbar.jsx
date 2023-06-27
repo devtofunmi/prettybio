d82b3598e74f48c77129e150c706402bfc8b6278
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-27 09:48:46 +0100

    add a style for the light mode

--- components/HomepageNavbar.jsx
+++ components/HomepageNavbar.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import GradientBorder from "./GradientBorder";
+import { useTheme } from "next-themes";
+
 const HomepageNavbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
   return (
-    <div className="flex items-center justify-between p-4 lg:p-8 text-text">
+    <div
+      className={`text ${
+        currentTheme === "dark" ? "text" : "black"
+      } flex items-center justify-between p-4 lg:p-8 font-abc mt-3 md:mt-0`}
+    >
       <div>
         <Link href="/">
-          <h2 className="text-2xl lg:text-3xl hover:text-textHover">PrettyBio</h2>
+          <h2 className="text-2xl lg:text-3xl">
+            PrettyBio
+          </h2>
         </Link>
       </div>
-      <div className='flex items-center justify-center'>
+      <div className="flex items-center justify-center">
         <div>
           <Link href="/Login">
             <button className="bg-transparent rounded-full text-sm lg:text-lg px-1 lg:px-4 py-2 ">
@@ -27,6 +45,6 @@ const HomepageNavbar = () => {
       </div>
     </div>
   );
-}
+};
 
-export default HomepageNavbar
+export default HomepageNavbar;
