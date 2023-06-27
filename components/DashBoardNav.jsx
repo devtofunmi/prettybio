6926b9e2c18750b3d2601d94b4eb01a530b255ff
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-27 23:56:37 +0100

    implement light and dark mode

--- components/DashBoardNav.jsx
+++ components/DashBoardNav.jsx
@@ -3,6 +3,8 @@ import { useTheme } from "next-themes";
 import GradientBorder from "./GradientBorder";
 import { supabase } from "../supabaseClient";
 import Link from "next/link";
+import Image from "next/image";
+
 
 const DashBoardNav = () => {
   const [userImage, setUserImage] = useState(null);
@@ -52,6 +54,11 @@ const DashBoardNav = () => {
     if (!mounted) return null;
     const currentTheme = theme === "system" ? systemTheme : theme;
 
+    const toggleTheme = () => {
+      const newTheme = currentTheme === "dark" ? "light" : "dark";
+      setTheme(newTheme);
+    };
+
   return (
     <div
       className={`${
@@ -60,11 +67,28 @@ const DashBoardNav = () => {
           : "flex items-center justify-between p-4 text-black shadow-lg sticky top-0"
       }`}
     >
-      <div>
+      <div className="md:flex md:gap-2 items-center">
         <div>
           <Link href="/">
-            <h2 className="text-3xl">PrettyBio</h2>
+            <h2 className="text-[20px] md:text-3xl">PrettyBio</h2>
           </Link>
+        </div>
+        <div>
+          <button
+            className={`bg-${
+              currentTheme === "dark" ? "black" : "gray"
+            }-700 hover:bg-${
+              currentTheme === "dark" ? "white" : "gray"
+            } w-[15px] md:w-[20px] rounded-md hidden md:block mt-2`}
+            onClick={toggleTheme}
+          >
+            <Image
+              src={`/assets/${currentTheme === "dark" ? "sun" : "moon"}.svg`}
+              alt="logo"
+              height={20}
+              width={20}
+            />
+          </button>
         </div>
         <div className="text-[10px] block md:hidden mt-2">
           <a
@@ -79,6 +103,21 @@ const DashBoardNav = () => {
         </div>
       </div>
       <div className="flex gap-[10px] items-center">
+        <button
+          className={`bg-${
+            currentTheme === "dark" ? "black" : "gray"
+          }-700 hover:bg-${
+            currentTheme === "dark" ? "white" : "gray"
+          } w-[15px] md:w-[20px] rounded-md block md:hidden mt-2`}
+          onClick={toggleTheme}
+        >
+          <Image
+            src={`/assets/${currentTheme === "dark" ? "sun" : "moon"}.svg`}
+            alt="logo"
+            height={20}
+            width={20}
+          />
+        </button>
         <div className="text-[13px] hidden md:block">
           <a
             href={`https://prettybio.netlify.app/${userLinkName}`}
