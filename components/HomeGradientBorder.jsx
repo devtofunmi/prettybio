a1865efc3fc5a45794a1ddc68f0a6425aebcc6c4
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-06-27 09:48:37 +0100

    add a style for the light mode

--- components/HomeGradientBorder.jsx
+++ components/HomeGradientBorder.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 const HomeGradientBorder = ({ children }) => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  const gradientStyle = {
+    background: "linear-gradient(275.79deg, #93c3c4 5.39%, #f2cfad 70.00%)",
+  };
+  const whiteGradientStyle = {
+    background: "linear-gradient(275.79deg, #d758bc  70.0%, #e8588e  30.39%)",
+  };
+
   return (
     <div
-      className="rounded-md p-0.5 fit-content flex items-center justify-center w-fit"
-      style={{
-        background:
-          "linear-gradient(275.79deg, #93c3c4  5.39%,#f2cfad   70.00%)",
-      }}
+      className={`style ${
+        currentTheme === "dark" ? "dark" : "light"
+      } rounded-md p-0.5 fit-content flex items-center justify-center w-fit`}
+      style={currentTheme === "dark" ? gradientStyle : whiteGradientStyle}
     >
-      <div className=" rounded-full flex items-center">{children}</div>
+      <div className="rounded-full flex items-center">{children}</div>
     </div>
   );
 };
