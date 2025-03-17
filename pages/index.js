e3ea8dbceb8d782aecdc73fbeefb9984c8ae225c
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-17 13:05:28 +0100

    clean the code up and import the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,11 @@
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import { useTheme } from 'next-themes';
-import Features from '../components/Features';
-import Footer from '../components/Footer';
-import Hero from '../components/Hero';
-import HomepageNavbar from '../components/HomepageNavbar';
-import Faq from '../components/Faq';
+import React from 'react';
+import LandingPage from './Landingpage';
 
 const Index = () => {
-  const { systemTheme, theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-  const currentTheme = theme === 'system' ? systemTheme : theme;
-
- 
 
   return (
-    <div className={`font-abc ${currentTheme === 'dark' ? 'dark' : ''}`}>
-
-      <HomepageNavbar />
-      <Hero />
-      <Features />
-      <Faq />
-      <Footer />
+    <div >
+      <LandingPage />
     </div>
   )
 };
