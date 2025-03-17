93715d413fb79b1823f5aa160385eca1d2ab4870
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-17 13:04:02 +0100

    .

--- components/UserInput.jsx.jsx
+++ components/UserInput.jsx.jsx
@@ -15,31 +15,24 @@ const UserInput = () => {
    const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
+    <div className="my-10 ">
     <div
       data-aos="fade-up-right"
-      className="my-10 flex w-full justify-center items-center m-auto"
+      className="flex w-full justify-center items-center m-auto"
     >
       <div className="flex items-center text-btntext  justify-center w-full  md:w-[500px] lg:w-1/2  px-5  my-auto mx-auto text-sm  lg:text-lg rounded-md mt-10">
         <HomeGradientBorder>
           <div
-            className={`${
-              currentTheme === "dark"
-                ? "bg-[#0c0c0c] text-text"
-                : "bg-[#f8f8f8] text-black"
-            } flex items-center  ml-1 pl-2 md:my-[2px] xm:text-[10px]  text-sm`}
+            className="bg-[#f8f8f8] text-black flex items-center  ml-1 pl-2 md:my-[2px] xm:text-[10px]  text-sm"
           >
-            <p className="py-3  text-[12px] md:text-base lg:text-xl ">
+            <p className="py-3  md:text-base lg:text-xl ">
               prettybio.com/
             </p>
             <input
               autocomplete="off"
               type="text"
               placeholder="yourname"
-              className={`${
-                currentTheme === "dark"
-                  ? "h-[2rem] md:h-[3rem] rounded-full  outline-none w-[50%] md:w-[60%] lg:w-[70%] text-[10px] lg:text-base bg-transparent px-0 md:px-0 text-text"
-                  : "h-[2rem] md:h-[3rem] rounded-full  outline-none w-[50%] md:w-[60%] lg:w-[70%] text-[10px] lg:text-base bg-transparent px-0 md:px-0 text-black"
-              } flex items-center  ml-1 pl-2 md:my-[2px] xm:text-[10px]  text-sm`}
+              className="h-[2rem] md:h-[3rem] rounded-full  outline-none w-[50%] md:w-[60%] lg:w-[70%] text-[10px] lg:text-base bg-transparent px-0 md:px-0 text-black flex items-center  ml-1 pl-2 md:my-[2px] xm:text-[10px]  text-sm"
             />
           </div>
           <Link href="/Signup">
@@ -49,6 +42,8 @@ const UserInput = () => {
           </Link>
         </HomeGradientBorder>
       </div>
+    </div>
+    <p className="text-center mt-5 text-gray-600">It’s free and takes just a minute to get started.</p>
     </div>
   );
 };
