338ea91d765412e87cfa044640d8a4b133fb30ac
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-18 18:51:29 +0100

    im gonna delete this component,i dont need this mf again

--- components/SignupNavbar.jsx
+++ components/SignupNavbar.jsx
@@ -1,46 +1,18 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
-import GradientBorder from "./GradientBorder";
+import React  from "react";
+
 
 const SignupNavbar = () => {
-   const { systemTheme, theme, setTheme } = useTheme();
-   const [mounted, setMounted] = useState(false);
-
-   useEffect(() => {
-     setMounted(true);
-   }, []);
-
-   if (!mounted) return null;
-   const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <div
-      className={`${
-        currentTheme === "dark"
-          ? "flex items-center justify-between p-4 lg:p-8 text-text"
-          : "flex items-center justify-between p-4 lg:p-8 text-black"
-      }`}
+      className="flex bg-white p-4 lg:p-8 "
     >
       <div>
         <Link href="/">
-          <h2 className="text-[20px] md:text-3xl">PrettyBio</h2>
+        <h2 className="text-3xl font-bold text-gray-800">PrettyBio</h2>
         </Link>
       </div>
-      <div>
-        <GradientBorder>
-          <Link href="/Login">
-            <button
-              className={`${
-                currentTheme === "dark"
-                  ? "bg-transparent text-sm lg:text-lg px-8 py-2 text-btntext"
-                  : "bg-transparent text-sm lg:text-lg px-8 py-2 text-black"
-              }`}
-            >
-              Log in
-            </button>
-          </Link>
-        </GradientBorder>
-      </div>
+    
     </div>
   );
 };
