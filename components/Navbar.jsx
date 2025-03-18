58556408f3fc2be65d1b7ce2a7756ce24296fdab
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-18 18:50:06 +0100

    created a new navbar for the login and signup page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,11 +5,11 @@ import React  from "react";
 const Navbar = () => {
   return (
     <div
-      className="flex bg-white p-4  "
+      className="flex bg-white "
     >
       <div>
         <Link href="/">
-        <h2 className="text-3xl font-bold text-gray-800">PrettyBio</h2>
+        <h2 className="text-2xl font-bold text-gray-800">PrettyBio</h2>
         </Link>
       </div>
     
