854a9f5ac79dc7dbbf2a0740a0f8cf4136b3745b
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-18 18:48:31 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com'],  // Include any other domains you're using
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      },
+    ],
   },
 };
+
+module.exports = nextConfig;
+
