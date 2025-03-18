510308b380363a706f488d721f48fd317be5ddca
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-18 18:30:43 +0100

    migrate code to typescript

---
+++ components/GradientBorder.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+
+interface GradientBorderProps {
+  children: ReactNode;
+}
+
+const GradientBorder: React.FC<GradientBorderProps> = ({ children }) => {
+  const whiteGradientStyle: React.CSSProperties = {
+    background: "linear-gradient(275.79deg, #d758bc  5.39%, #e8588e  70.39%)",
+  };
+
+  return (
+    <div
+      className="p-0.5 fit-content flex items-center justify-center w-full"
+      style={whiteGradientStyle}
+    >
+      <div className="flex items-center">{children}</div>
+    </div>
+  );
+};
+
+export default GradientBorder;
+
