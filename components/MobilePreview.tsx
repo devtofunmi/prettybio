1d13d5079a236eaa5d4c1982238eafa4e46a9a98
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-22 23:24:53 +0100

    componet to preview the linkpage

---
+++ components/MobilePreview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface MobilePreviewProps {
+  userLinkName: string;
+}
+
+const MobilePreview: React.FC<MobilePreviewProps> = ({ userLinkName }) => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="bg-gray-200 border-5 border-gray-700 rounded-2xl overflow-hidden 
+        max-w-[250px] h-[50vh] lg:w-[250px] lg:h-[500px] 
+        lg:shadow-lg shadow-none">
+        <iframe
+          scrolling="no"
+          className="w-full h-full"
+          src={`https://prettybio.netlify.app/${userLinkName}`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MobilePreview;
+
+  
+  
+  
+  
+  
