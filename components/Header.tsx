22109dbbdaf5feb0c85dc899a3c053d1a4839957
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-03-29 23:50:14 +0100

    change the route

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,20 +11,20 @@ const Header: FC = () => {
           <h2 className="text-3xl font-bold text-gray-800">PrettyBio</h2>
         </Link>
         <nav className="hidden md:flex flex-1 justify-center items-center gap-12">
-          <Link href="/features" className="px-4 py-2 text-gray-800">
+          <Link href="/landingpage/features" className="px-4 py-2 text-gray-800">
             Features
           </Link>
-          <Link href="/faq" className="px-4 py-2 text-gray-800">
+          <Link href="/landingpage/faq" className="px-4 py-2 text-gray-800">
             FAQ
           </Link>
         </nav>
 
         <div className="hidden md:flex items-center gap-6">
-          <Link href="/Login" className="px-4 py-2 text-gray-800">
+          <Link href="/authentication/Login" className="px-4 py-2 text-gray-800">
             Login
           </Link>
           <Link
-            href="/Signup"
+            href="/authentication/Signup"
             className="bg-gray-900 text-white px-6 py-2 rounded-full hover:bg-gray-800"
           >
             Sign Up
@@ -44,17 +44,17 @@ const Header: FC = () => {
       {menuOpen && (
         <nav className="fixed z-10 top-16 left-0 w-full h-screen bg-white shadow-md font-semibold flex flex-col px-4 py-4 md:hidden">
           <div className="flex flex-col items-left gap-10">
-            <Link href="/features" className="block px-4 py-2 text-2xl mt-5 text-gray-800">
+            <Link href="/landingpage/features" className="block px-4 py-2 text-2xl mt-5 text-gray-800">
               Features
             </Link>
-            <Link href="/faq" className="block px-4 py-2 text-2xl text-gray-800">
+            <Link href="/landingpage/faq" className="block px-4 py-2 text-2xl text-gray-800">
               FAQ
             </Link>
-            <Link href="/Login" className="block px-4 py-2 text-2xl text-gray-800">
+            <Link href="/authentication/Login" className="block px-4 py-2 text-2xl text-gray-800">
               Login
             </Link>
             <Link
-              href="/Signup"
+              href="/authentication/Signup"
               className="block text-gray-800 px-4 py-4 text-2xl rounded-full hover:bg-gray-800"
             >
               Sign Up
