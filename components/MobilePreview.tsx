918109a727fc1198a73f4d98f97650e63acf749a
DevTofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-25 09:26:20 +0100

    Update MobilePreview.tsx

--- components/MobilePreview.tsx
+++ components/MobilePreview.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 
 interface MobilePreviewProps {
-  userLinkName: string;
+userLinkName: string;
 }
 
 const MobilePreview: React.FC<MobilePreviewProps> = ({ userLinkName }) => {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <div className="bg-gray-200 border-5 border-gray-700 rounded-2xl overflow-hidden 
-        w-[200px] h-[250px] lg:w-[200px] lg:h-[250px] 
-        lg:shadow-lg shadow-none ">
-        <iframe
-          scrolling="no"
-          className="w-full h-full"
-          src={`https://prettybio.netlify.app/${userLinkName}`}
-        />
-      </div>
-    </div>
-  );
+return (
+<div className="flex justify-center items-center h-full">
+<div className="bg-gray-200 border-5 border-gray-700 rounded-2xl overflow-hidden   
+max-w-[250px] h-[50vh] lg:w-[250px] lg:h-[500px]   
+lg:shadow-lg shadow-none ">
+<iframe
+scrolling="no"
+className="w-full h-full"
+src={https://prettybio.netlify.app/${userLinkName}}
+/>
+</div>
+</div>
+);
 };
 
 export default MobilePreview;
+
