b52cf52cc9d1ac73d6f5f043712c657ba0802a11
DevTofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-25 02:36:55 +0100

    change perview from 1.2 to 1

--- pages/landingpage/Landingpage.tsx
+++ pages/landingpage/Landingpage.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
     renderMode: "performance",
     breakpoints: {
       "(max-width: 1024px)": { slides: { perView: 3 } },
-      "(max-width: 640px)": { slides: { perView: 1.2 } },
+      "(max-width: 640px)": { slides: { perView: 1 } },
     },
     created(slider) {
       let timeout: NodeJS.Timeout;
