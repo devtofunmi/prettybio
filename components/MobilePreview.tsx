c562dfdda3e96753b20506ddb689bfcfe4f5d89b
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-25 20:40:28 +0100

    fixed the w to fit

--- components/MobilePreview.tsx
+++ components/MobilePreview.tsx
@@ -8,7 +8,7 @@ const MobilePreview: React.FC<MobilePreviewProps> = ({ userLinkName }) => {
   return (
     <div className="flex justify-center items-center h-full">
       <div className="relative bg-[#f5f5f5] w-[300px] h-[500px] md:h-[500px] rounded-[2.5rem] shadow-2xl overflow-hidden border border-gray-300">
-        <div className="w-full h-screen overflow-auto scrollbar-hide">
+        <div className="w-fit h-screen overflow-auto scrollbar-hide">
           <iframe
             title="Mobile Preview"
             src={`https://prettybio.netlify.app/${userLinkName}`}
